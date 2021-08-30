8770e5c060d738d2a26ee60b9bb5edcec73b3ad3
Sofja Yefremova <sofjarammstein@gmail.com>
2021-08-31 00:02:12 +0300

    Added test stubs JUnit5 for Model class.

---
+++ MoreOrLess/src/test/java/ModelTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelTest {
+
+    @org.junit.jupiter.api.Test
+    void setSecretValue() {
+        // TODO: implement this
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkValue() {
+        // TODO: implement this
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPrimaryBarrier() {
+        // TODO: implement this
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSecretValue() {
+        // TODO: implement this
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMinBarrier() {
+        // TODO: implement this
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMaxBarrier() {
+        // TODO: implement this
+    }
+
+    @org.junit.jupiter.api.Test
+    void getYourWay() {
+        // TODO: implement this
+    }
+}
