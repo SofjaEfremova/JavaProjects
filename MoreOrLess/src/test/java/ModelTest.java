f0939bdca343e15b2f68e59016bb98235fcaf9e3
Sofja Yefremova <sofjarammstein@gmail.com>
2021-08-31 16:56:08 +0300

    Implemented tests for the Model class

--- MoreOrLess/src/test/java/ModelTest.java
+++ MoreOrLess/src/test/java/ModelTest.java
@@ -1,39 +1,111 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
 
     @org.junit.jupiter.api.Test
     void setSecretValue() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        for (int i = 0; i <= 100; i++ ) {
+            model.setSecretValue();
+            int secretValue = model.getSecretValue();
+
+            assertTrue(secretValue > GlobalConstants.PRIMARY_MIN_BARRIER);
+            assertTrue(secretValue < GlobalConstants.PRIMARY_MAX_BARRIER);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void checkValue() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        model.setSecretValue();
+        int secretValue = model.getSecretValue();
+        List<Integer> expectedWay = new ArrayList<Integer>();
+
+        expectedWay.add(secretValue + 1);
+        assertTrue(model.checkValue(secretValue + 1));
+        assertEquals(secretValue + 1, model.getMaxBarrier());
+
+        expectedWay.add(secretValue - 1);
+        assertTrue(model.checkValue(secretValue - 1));
+        assertEquals(secretValue - 1, model.getMinBarrier());
+
+        expectedWay.add(secretValue);
+        assertFalse(model.checkValue(secretValue));
+
+        assertEquals(expectedWay, model.getYourWay());
     }
 
     @org.junit.jupiter.api.Test
     void setPrimaryBarrier() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        assertEquals(GlobalConstants.PRIMARY_MIN_BARRIER, model.getMinBarrier());
+        assertEquals(GlobalConstants.PRIMARY_MAX_BARRIER, model.getMaxBarrier());
     }
 
     @org.junit.jupiter.api.Test
     void getSecretValue() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        model.setSecretValue();
+        int secretValue = model.getSecretValue();
+
+        assertTrue(secretValue > GlobalConstants.PRIMARY_MIN_BARRIER);
+        assertTrue(secretValue < GlobalConstants.PRIMARY_MAX_BARRIER);
     }
 
     @org.junit.jupiter.api.Test
     void getMinBarrier() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        assertEquals(GlobalConstants.PRIMARY_MIN_BARRIER, model.getMinBarrier());
     }
 
     @org.junit.jupiter.api.Test
     void getMaxBarrier() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        assertEquals(GlobalConstants.PRIMARY_MAX_BARRIER, model.getMaxBarrier());
     }
 
     @org.junit.jupiter.api.Test
     void getYourWay() {
-        // TODO: implement this
+        Model model = new Model();
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        model.setSecretValue();
+        int secretValue = model.getSecretValue();
+        List<Integer> expectedWay = new ArrayList<Integer>();
+
+        expectedWay.add(secretValue + 1);
+        assertTrue(model.checkValue(secretValue + 1));
+        assertEquals(secretValue + 1, model.getMaxBarrier());
+
+        expectedWay.add(secretValue - 1);
+        assertTrue(model.checkValue(secretValue - 1));
+        assertEquals(secretValue - 1, model.getMinBarrier());
+
+        expectedWay.add(secretValue);
+        assertFalse(model.checkValue(secretValue));
+
+        assertEquals(expectedWay, model.getYourWay());
     }
 }
